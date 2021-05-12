9ff8e8be2e9e7c4f860f6f97c92ac64399d078ed
2000503 <0117Qwer!>
2021-05-12 16:10:15 +0900

    수정

--- src/main/java/com/tutorial/SpringRestApiApplication.java
+++ src/main/java/com/tutorial/SpringRestApiApplication.java
@@ -5,6 +5,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
+// test 20210512
 @SpringBootApplication
 public class SpringRestApiApplication {
 
